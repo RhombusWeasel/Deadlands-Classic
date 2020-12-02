d3d736d21e144b263b60604d238050b1c6e63098
Pete Hunter <pete.hunter@piksel.com>
2020-12-02 17:04:17 +0000

    Minimal styling for sheets

--- module/sheets/actor.js
+++ module/sheets/actor.js
@@ -2,7 +2,7 @@ export default class PlayerSheet extends ActorSheet {
     static get defaultOptions() {
         return mergeObject(super.defaultOptions, {
             template: `systems/deadlands_classic/templates/player-sheet.html`,
-//            classes: ["player-sheet"]
+            classes: ["player-sheet"]
         });
     }
 }
