66d66b3c46b2922fce7162e1a6dee424a7aed291
Pete Hunter <pete.hunter@piksel.com>
2020-12-03 20:37:23 +0000

    Added attack and reload buttons to firearms.

--- module/sheets/actor.js
+++ module/sheets/actor.js
@@ -22,6 +22,8 @@ export default class PlayerSheet extends ActorSheet {
     activateListeners(html) {
         html.find(".info-button").click(this._on_item_open.bind(this));
         html.find(".melee-attack").click(this._on_melee_attack.bind(this));
+        html.find(".gun-attack").click(this._on_gun_attack.bind(this));
+        html.find(".gun-reload").click(this._on_gun_reload.bind(this));
         return super.activateListeners(html);
     }
 
@@ -29,7 +31,6 @@ export default class PlayerSheet extends ActorSheet {
         event.preventDefault();
         let element = event.currentTarget;
         let itemId = element.closest(".item").dataset.itemid;
-        console.log(element.closest(".item").dataset)
         let item = this.actor.getOwnedItem(itemId);
         item.sheet.render(true);
     }
@@ -39,7 +40,6 @@ export default class PlayerSheet extends ActorSheet {
         let element = event.currentTarget;
         let itemId = element.closest(".item").dataset.itemid;
         let item = this.actor.getOwnedItem(itemId);
-        console.log(item)
         let dmg = item.data.data.damage;
         let act = this.getData();
         let trait = act.data.traits.nimbleness;
@@ -48,12 +48,63 @@ export default class PlayerSheet extends ActorSheet {
         if (lvl == 0) {
             lvl = trait.level
         }
-        let roll = `
-            Brawlin: [[${lvl}${trait.die_type}ex + ${trait.modifier} + ${skill.modifier}]] \n
-            Damage: [[${act.data.traits.strength.level}${act.data.traits.strength.die_type}x= + ${dmg}x=]] \n
-            Location: [[d20]] \n
-        `;
+        let roll = `Brawlin: [[${lvl}${trait.die_type}ex + ${trait.modifier} + ${skill.modifier}]]\nDamage: [[${act.data.traits.strength.level}${act.data.traits.strength.die_type}x= + ${dmg}x=]]\nLocation: [[d20]]`;
         ChatMessage.create({ content: roll});
-        console.log(item)
+    }
+
+    _on_gun_attack(event) {
+        event.preventDefault();
+        let element = event.currentTarget;
+        let itemId = element.closest(".item").dataset.itemid;
+        let item = this.actor.getOwnedItem(itemId);
+        let shots = item.data.data.chamber;
+        let dmg = item.data.data.damage;
+        let act = this.getData();
+        let trait = act.data.traits.deftness;
+        let skill = trait.skills["shootin_".concat(item.data.data.gun_type)];
+        if (shots > 0) {
+            let lvl = skill.level
+            if (lvl == 0) {
+                lvl = trait.level
+            }
+            let roll = `
+                Shootin: [[${lvl}${trait.die_type}ex + ${trait.modifier} + ${skill.modifier}]]\n
+                Damage: [[${dmg}x=]]\n
+                Location: [[d20]]
+            `;
+            ChatMessage.create({ content: roll});
+            shots = shots - 1;
+        }else{
+            ChatMessage.create({ content: 'Click... Click Click! Looks like you\'re empty partner'});
+        }
+        item.update({ "data.chamber": shots});
+    }
+
+    _on_gun_reload(event) {
+        event.preventDefault();
+        let element = event.currentTarget;
+        let itemId = element.closest(".item").dataset.itemid;
+        let item = this.actor.getOwnedItem(itemId);
+        let shots = item.data.data.chamber;
+        let max = item.data.data.max;
+        let act = this.getData();
+        let trait = act.data.traits.deftness;
+        let skill = trait.skills.speed_load;
+        let lvl = skill.level;
+        let roll = `${lvl}${trait.die_type}ex + ${skill.modifier} + ${trait.modifier}`
+        if (lvl < 1){
+            roll = `${trait.level}${trait.die_type}ex + ${skill.modifier} + ${trait.modifier}`
+        }
+        let reply = 'You failed your speed load skill check and manage to get 1 bullet into the gun.'
+        let r = new Roll(roll).roll()
+        r.toMessage()
+        if(r._total > 5){
+            reply = 'You passed your speed load skill check and manage to cram your gun full of bullets!'
+            shots = max
+        }else{
+            shots = shots + 1
+        }
+        item.update({ "data.chamber": shots});
+        ChatMessage.create({ content: reply});
     }
 }
