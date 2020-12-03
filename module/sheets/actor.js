bb377fce5db6cd2824cf6ce7eb3bdb217ebea8eb
Pete Hunter <pete.hunter@piksel.com>
2020-12-03 09:42:37 +0000

    Updated sidebar to show player items.

--- module/sheets/actor.js
+++ module/sheets/actor.js
@@ -13,8 +13,25 @@ export default class PlayerSheet extends ActorSheet {
         data.melee_weapons = data.items.filter(function (item) {return item.type == "melee"});
         data.miracles = data.items.filter(function (item) {return item.type == "miracle"});
         data.hexes = data.items.filter(function (item) {return item.type == "hex"});
-        data.favours = data.items.filter(function (item) {return item.type == "favor"});
+        data.favors = data.items.filter(function (item) {return item.type == "favor"});
+        data.hinderances = data.items.filter(function (item) {return item.type == "hinderance"});
+        data.edges = data.items.filter(function (item) {return item.type == "edge"});
 
         return data;
     }
+
+    // activateListeners(html) {
+    //     html.find(".inline-edit").change(this._on_entry_change.bind(this))
+    //     return super.activateListeners(html);
+    // }
+
+    // _on_entry_changed(event) {
+    //     event.preventDefault();
+    //     let element = event.currentTarget;
+    //     let itemId = element.closest(".item").dataset.itemId;
+    //     let item = this.actor.getOwnedItem(itemId);
+    //     let field = element.dataset.field;
+
+    //     return item.update({ [field]: element.value });
+    // }
 }
