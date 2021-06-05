eccaf5a5829be5b0b07ad233e1cba9d38a13ca34
Pete Hunter <pete.hunter@piksel.com>
2021-06-05 20:33:38 +0100

    Removed excess logging.

--- module/deck.js
+++ module/deck.js
@@ -464,36 +464,37 @@ let operations = {
     },
     discard_card: function(data) {
         if (game.user.isGM) {
-            let joker = `https://opengameart.org/sites/default/files/styles/medium/public/oga-textures/92832/hearts_joker.png`;
+            let content = `
+            <style>
+                body  {
+                    background-image: url("${game.dc.url_dict[data.name]}");
+                }
+            </style>
+            <div class="body">
+            `;
             if (data.name == "Joker (Black)") {
-                ChatMessage.create({ content: `
-                    <div>
-                        <style>
-                            body  {
-                                background-image: url("${game.dc.url_dict[data.name]}");
-                            }
-                        </style>
+                content += `
                         <h3 style="text-align: center;">Black Joker!</h3>
                         <p style="text-align: center;">You lose your next highest card.</p>
                         <p style="text-align: center;">The combat deck will be reshuffled at the end of the round.</p>
                         <p style="text-align: center;">The Marshal draws a Fate Chip.</p>
-                    </div>
-                `});
+                `;
                 game.dc.combat_shuffle = true
             }else if (data.name == "Joker (Red)") {
-                ChatMessage.create({ content: `
+                content += `
                     <h3 style="text-align: center;">Red Joker!</h3>
                     <img src="${game.dc.url_dict[data.name]}"></img>
                     <p style="text-align: center;">You may act first or if sleeved may interrupt any card.</p>
                     <p style="text-align: center;">${data.char} may draw a Fate Chip.</p>
-                `});
+                `;
             }else{
-                ChatMessage.create({ content: `
+                content += `
                     <h3 style="text-align: center;">Action Deck</h3>
                     <img src="${game.dc.url_dict[data.name]}"></img>
                     <p style="text-align: center;">${data.char} plays ${data.name}</p>
-                `});
+                `;
             }
+            ChatMessage.create({content: content});
             game.dc.action_discard.push(data)
         }
     },
