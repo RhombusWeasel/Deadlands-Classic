41bc1cf1ddb661ce7e761abc6c0e5828abb9a795
Pete Hunter <pete.hunter@piksel.com>
2021-07-20 12:49:31 +0100

    Html updates, player sheet.

--- module/sheets/actor.js
+++ module/sheets/actor.js
@@ -413,8 +413,8 @@ export default class PlayerSheet extends ActorSheet {
                         <h3 style="text-align:center">Bounty: ${chip_type}</h3>
                         <p style="text-align:center">${this.actor.name.split(' ')[0]} gains ${bounty} bounty ${suffix}.</p>
                     `});
-                    this.actor.data.update({"data.data.bounty.value": new_val});
-                    this.actor.data.update({"data.data.bounty.max": new_max});
+                    this.actor.data.update({data: {data: {bounty: {value: new_val}}}});
+                    this.actor.data.update({data: {data: {bounty: {max: new_max}}}});
                     this.actor.deleteOwnedItem(chip._id);
                     found = true;
                 }
