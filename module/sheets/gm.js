755d35af134acc8d6f07fad1f589195e425856fd
Pete Hunter <pete.hunter@piksel.com>
2021-07-20 18:24:28 +0100

    HTML updates: GM Sheet.

--- module/sheets/gm.js
+++ module/sheets/gm.js
@@ -168,7 +168,7 @@ export default class GMSheet extends ActorSheet {
         let element = event.currentTarget;
         let chip_type = element.closest(".use-fate").dataset.chip;
         let act = this.getData();
-        let fate_chips = dc_utils.char.items.get("chip");
+        let fate_chips = dc_utils.char.items.get(act, "chip");
         let responses = [
             `I think you might've pissed 'im off`,
             `Let's hope he doesn't have it in fer ya.`,
