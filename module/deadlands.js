e60811b2f197625dfcd29d50bb442b345137e475
Pete Hunter <pete.hunter@piksel.com>
2021-07-22 11:52:32 +0100

    Added equipment partial.

--- module/deadlands.js
+++ module/deadlands.js
@@ -88,7 +88,7 @@ Hooks.once("init", function () {
     });
 
     Handlebars.registerHelper('if_equipped', function (slot, id, options) {
-        //console.log(id, type, val, options);
+        console.log(id, slot, options);
         let act = game.actors.get(options.data.root.id);
         if (dc_utils.char.items.is_equipped(act, slot, id)) {
             return options.fn(this);
