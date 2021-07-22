870d9b264ebedac552188e66f690fce9d0a0815f
Pete Hunter <pete.hunter@piksel.com>
2021-07-22 19:05:17 +0100

    Changed file structure a little.

--- module/deadlands.js
+++ module/deadlands.js
@@ -100,7 +100,7 @@ Hooks.once("init", function () {
     Handlebars.registerHelper('is_one_handed', function(options) {
         let act = game.actors.get(options.data.root.id);
         let item = act.items.get(act.data.data.equipped.dominant);
-        console.log('DC | HBH is_one_handed |', act, item);
+        console.log('DC | HBH is_one_handed |', item.data.data.multi_slot);
         if (item.data.data.multi_slot == false) {
             return options.fn(this);
         }
