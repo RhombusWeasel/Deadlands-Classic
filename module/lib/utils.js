58e7353eca152f68c9e675160b721250708cd41f
Pete Hunter <pete.hunter@piksel.com>
2021-07-23 01:31:28 +0100

    Changed roll structure.

--- module/lib/utils.js
+++ module/lib/utils.js
@@ -177,7 +177,7 @@ const dc_utils = {
     roll: {
         new_roll_packet: function(act, type, skl, wep) {
             let item = act.items.get(wep);
-            let dist = 0
+            let dist = 1
             if (!(item)) {
                 wep = 'unarmed'
             }
