d11394c190baef1862c9c3db6a874bd7dd06aca8
Pete Hunter <pete.hunter@piksel.com>
2021-07-24 23:24:09 +0100

    Updated equip functionality.

--- module/sheets/item.js
+++ module/sheets/item.js
@@ -8,4 +8,14 @@ export default class DCItem extends ItemSheet {
         data.config = CONFIG.dc;
         return data;
     }
+
+    activateListeners(html) {
+        html.find(".add-modifier").click(this._on_add_modifier.bind(this));
+        return super.activateListeners(html);
+    }
+
+    _on_add_modifier(event) {
+        event.preventDefault()
+        console.log(this);
+    }
 }
