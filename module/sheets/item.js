4824eb07c2e39acc5f4f37ccd44426aef1a4c555
Pete Hunter <pete.hunter@piksel.com>
2021-07-25 01:07:24 +0100

    Updated equip functionality.

--- module/sheets/item.js
+++ module/sheets/item.js
@@ -6,7 +6,7 @@ export default class DCItem extends ItemSheet {
     getData() {
         const data = super.getData();
         data.config = CONFIG.dc;
-        data.modifiers = this.object.data.data.modifiers
+        data.modifiers = game.items.find(i => function(){return i.name == this.object.name});
         console.log(this);
         return data;
     }
@@ -17,13 +17,16 @@ export default class DCItem extends ItemSheet {
     }
 
     _on_add_modifier(event) {
-        event.preventDefault()
+        event.preventDefault();
         this.object.data.data.modifiers.push({
             type: 'skill_mod',
             target: 'cognition',
             modifier: 2
-        })
-        this.object.update(this.object.data);
+        });
+        let data = {
+            modifiers: this.data.data.modifiers
+        }
+        this.object.update(data);
         console.log(this);
         return this.getData();
     }
