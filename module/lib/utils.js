bb0d682b2887d60f6181c12ff434a7dfa8c97b31
Pete Hunter <pete.hunter@piksel.com>
2021-08-18 19:17:47 +0100

    Flush calculation.

--- module/lib/utils.js
+++ module/lib/utils.js
@@ -905,11 +905,19 @@ const dc_utils = {
                 }
                 // Flushes
                 for (let c_1 = 0; c_1 < cards.length - 6; c_1++) {
-                    let str = '';
+                    let str = 'Flush';
                     for (let c_2 = c_1; c_2 < c_1 + 4; c_2++) {
                         str += ` ${cards[c_2]}`;
                     }
-                    hands.push(`Flush${str} ${cards[c_1 + 5]}`);
+                    hands.push(str + `${cards[c_2 + 5]}`);
+                    for (let i = 1; i < 6; i++) {
+                        str = 'Flush'
+                        for (let c_2 = c_1; c_2 < c_1 + 6; c_2++) {
+                            if (c_2 - c_1 != i) {
+                                str += ` ${cards[c_2]}`;
+                            }
+                        }
+                    }
                 }
                 // Straights
                 for (let c = 0; c < cards.length - 4; c++) {
