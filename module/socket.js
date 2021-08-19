8e9e302a858679338956574a8e5b2157b929b3b5
Pete Hunter <pete.hunter@piksel.com>
2021-08-20 00:38:49 +0100

    0.8.9 update.

--- module/socket.js
+++ module/socket.js
@@ -503,7 +503,7 @@ let operations = {
     apply_hit: function(data) {
         if (game.user.isGM) {
             let act = dc_utils.get_actor(data.attacker);
-            let raises    = Math.floor((data.attack_roll - 5) / 5);
+            let raises    = Math.floor((data.hit_roll - 5) / 5);
             let called    = act.data.data.called_shot;
             data.location = dc_utils.roll.location_roll(raises, called);
             data.loc_label = dc_utils.hit_locations[data.location];
