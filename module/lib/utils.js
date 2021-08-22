251c1a809e78ae7fdbac7d0b3d00aa856d1b1de6
Pete Hunter <pete.hunter@piksel.com>
2021-08-23 00:36:48 +0100

    Start of poker.

--- module/lib/utils.js
+++ module/lib/utils.js
@@ -1847,8 +1847,10 @@ const dc_utils = {
                 for (let i = tot - range; i < tot + range; i++) {
                     console.log(i, dc_utils.loc_lookup[i]);
                     if (i <= 19){
-                        if (!(found.includes(dc_utils.loc_lookup[i]))) {
-                            found.push(dc_utils.loc_lookup[i]);
+                        if (dc_utils.loc_lookup[i]) {
+                            if (!(found.includes(dc_utils.loc_lookup[i]))) {
+                                found.push(dc_utils.loc_lookup[i]);
+                            }
                         }
                     }
                 }
