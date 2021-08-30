fd72bf8127c1974a7b5cd277c05268dc8079568b
Pete Hunter <pete.hunter@piksel.com>
2021-08-30 21:36:58 +0100

    Poker update.

--- module/deadlands.js
+++ module/deadlands.js
@@ -220,6 +220,7 @@ Hooks.on('preCreateToken', function (document, createData, options, userId) {
             name = dc_utils.char.random_name(eth, 'female');
         }
         document.data.update({name: name});
+        // This is stupid and wrong, don't be like me.
         setTimeout(() => {
             canvas.tokens.placeables.find(i => i.name == name).document.actor.update({name: name});
         }, 500);
