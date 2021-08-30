51dd64feb3774446c95793806088adc604dd7cb7
Pete Hunter <pete.hunter@piksel.com>
2021-08-30 22:09:41 +0100

    Poker update.

--- module/lib/utils.js
+++ module/lib/utils.js
@@ -1121,7 +1121,8 @@ const dc_utils = {
                 let sn = Math.floor(Math.random() * dc_utils.names.american.surnames.length)
                 return `${dc_utils.names.american.forenames[sex][fn]} ${dc_utils.names.american.surnames[sn]}`
             }else if (eth == 'native_american') {
-                let fn = Math.floor(Math.random() * dc_utils.names.native_american.forenames[sex].length)
+                let fn = Math.floor(Math.random() * dc_utils.names.native_american.forenames[sex].length);
+                return `${fn}`;
             }
         },
         has: function(act, type, name) {
