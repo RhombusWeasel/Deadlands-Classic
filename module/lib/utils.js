fc9a11ed9369ba004a8e8efb7da16248679f6cc0
Pete Hunter <pete.hunter@piksel.com>
2021-10-06 17:42:56 +0100

    Updated hexes tab - bugfix.

--- module/lib/utils.js
+++ module/lib/utils.js
@@ -1783,14 +1783,12 @@ const dc_utils = {
                         }
                     }
                 }
-                console.log('roll_damage: Location:', found, found.length - 1);
                 if (found.includes('noggin')) {
                     return 'noggin';
                 }else if (found.includes('gizzards')) {
                     return 'gizzards';
                 }
                 loc_key = found[found.length - 1];
-                console.log('roll_damage: Location:', loc_key);
             }else{
                 let locs = dc_utils.called_shots[key].locations
                 loc_key = locs[Math.floor(Math.random() * locs.length)]
