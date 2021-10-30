d8ca69981ef1fc82db88af4e936c4613076aec0e
Pete Hunter <pete.hunter@piksel.com>
2021-10-30 15:21:27 +0100

    Starting passing items.

--- module/lib/utils.js
+++ module/lib/utils.js
@@ -1146,7 +1146,7 @@ const dc_utils = {
     },
     user: {
         get_owned_actors: function() {
-            return game.actors.filter(i => i.owner == true);
+            return game.actors.filter(i => i.isOwner == true);
         },
     },
     char: {
