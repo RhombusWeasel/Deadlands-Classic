2891f6ffea0e5d5e00fb8d2aa1ff427dafa71fda
pete.hunter <pete.hunter@piksel.com>
2021-11-09 23:54:27 +0000

    Merchants 0.1.7.17

--- module/sheets/merchant.js
+++ module/sheets/merchant.js
@@ -40,9 +40,9 @@ export default class MerchantSheet extends actor_sheet {
                 buy_itm.data.boxed_multiple && pl_item.data.data.amount >= buy_itm.data.box_amount
             )));
             // Filter for items in the current trade
-            data.cust_melee   = data.cust_melee.filter(pl_item => !data.current_trade.sell.some(buy_itm => pl_item.id == buy_itm.id));
-            data.cust_guns    = data.cust_guns.filter(pl_item => !data.current_trade.sell.some(buy_itm => pl_item.id == buy_itm.id));
-            data.cust_goods   = data.cust_goods.filter(pl_item => !data.current_trade.sell.some(buy_itm => pl_item.id == buy_itm.id));
+            data.cust_melee   = data.cust_melee.filter(pl_item => !data.current_trade.sell.some(buy_itm => pl_item._id == buy_itm._id));
+            data.cust_guns    = data.cust_guns.filter(pl_item => !data.current_trade.sell.some(buy_itm => pl_item._id == buy_itm._id));
+            data.cust_goods   = data.cust_goods.filter(pl_item => !data.current_trade.sell.some(buy_itm => pl_item._id == buy_itm._id));
         }
         return data;
     }
