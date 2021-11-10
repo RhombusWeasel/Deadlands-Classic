1ab2e6e8a01f8e00000550a49fecc589b6076b68
pete.hunter <pete.hunter@piksel.com>
2021-11-10 17:40:09 +0000

    Merchants 0.1.7.22

--- module/sheets/merchant.js
+++ module/sheets/merchant.js
@@ -182,6 +182,7 @@ export default class MerchantSheet extends actor_sheet {
         let customers = this.actor.data.data.customers;
         for (let i = 0; i < customers[p_name].current.trade.sell.length; i++) {
             const item = customers[p_name].current.trade.sell[i];
+            console.log(item);
             let price  = parseFloat(item.data.data.cost.slice(1, item.data.data.cost.length));
             customers[p_name].current.trade.total -= (price * this.actor.data.data.buy_modifier);
         }
