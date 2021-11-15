0404d30f0549842ea32c5629f1fe67cad51fb6a2
pete.hunter <pete.hunter@piksel.com>
2021-11-15 17:54:21 +0000

    GM Tab 0.1.6

--- module/sheets/merchant.js
+++ module/sheets/merchant.js
@@ -7,7 +7,7 @@ export default class MerchantSheet extends actor_sheet {
                 template: `systems/deadlands_classic/templates/sheets/merchant.html`,
                 classes: ["player-sheet", "doc"],
                 width: 800,
-                height: 650
+                height: 670
             });
         }else{
             console.log('merchant.js: User is GM, launching Player Sheet')
