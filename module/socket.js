130b286ded67a63506fd7930778d91965998ea59
pete.hunter <pete.hunter@piksel.com>
2021-11-23 21:42:17 +0000

    GM Sheet 0.2.19

--- module/socket.js
+++ module/socket.js
@@ -772,7 +772,7 @@ Hooks.on("ready", () => {
         if (data.operation in operations) {
             console.log('RECIEVE:', data.operation, data.data);
             operations[data.operation](data.data);
-            dc_utils.gm.update_sheet();
+            setTimeout(() => {dc_utils.gm.update_sheet()}, Math.random() * 1000);
             return false;
         }
     });
