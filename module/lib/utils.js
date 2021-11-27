e6f88206f30c191971bd0fee337e4271371d862b
pete.hunter <pete.hunter@piksel.com>
2021-11-27 17:51:51 +0000

    GM Sheet 0.5.13

--- module/lib/utils.js
+++ module/lib/utils.js
@@ -1546,10 +1546,20 @@ const dc_utils = {
                         act.update({data: {wound_soak: 0}});
                     }
                     return setTimeout(() => {
-                        act.update({data: {wounds: {[loc]: tot}}});
+                        let data = {
+                            wounds: {
+                                [loc]: tot
+                            },
+                        }
                         dc_utils.chat.send('Wound', `${act.name} takes ${dc_utils.pluralize(amt, 'wound', 'wounds')} to the ${dc_utils.hit_locations[loc]}`);
                         dc_utils.char.wounds.calculate_wound_modifier(act, amt);
                         dc_utils.char.wounds.apply_wind_damage(act, tot);
+                        if (act.data.data.heals[loc] == 0) {
+                            let timestamp = game.settings.get('deadlands_classic', 'unixtime');
+                            let next_heal = timestamp + act.data.data.healing_factor
+                            data.heals = {[loc]: next_heal}
+                        }
+                        act.update({data});
                     }, Math.random() * 500);
                 }else{
                     dc_utils.chat.send('Wound', `${act.name} was lucky they just winged 'em.`);
