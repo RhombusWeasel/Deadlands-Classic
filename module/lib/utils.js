dbd1876193f6b8218dddd3393b7dafb88265b97c
RhombusWeasel <pete@Jarvis.localdomain>
2021-12-11 00:35:20 +0000

    Documents 0.6.10

--- module/lib/utils.js
+++ module/lib/utils.js
@@ -2885,7 +2885,6 @@ const dc_utils = {
                 }
                 return `
                     <div>
-                        <img src="https://cdn.pixabay.com/photo/2014/04/02/10/24/stamp-303749_960_720.png">
                         <div class="${cl}-date"><p>${data.date}</p></div>
                         <div class="${cl}"><p>${data.greeting}</p></div>
                         <div class="${cl}"><p>${data.body}</p></div>
@@ -2903,8 +2902,8 @@ const dc_utils = {
                     cl = 'letter';
                 }
                 return `
-                    <div class="typed">
-                        
+                    <div class="telegram">
+                        <img class="telegram-stamp" src="https://cdn.pixabay.com/photo/2014/04/02/10/24/stamp-303749_960_720.png">
                         <p class="${cl}-date">${data.date}</p>
                         <p class="telegram-header">${data.company}</p>
                         <p class="${cl}">${data.reciever}</p>
