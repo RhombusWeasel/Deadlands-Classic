e86819a39fd26f72a7815ae3d8bf3030a9b2b890
RhombusWeasel <pete@Jarvis.localdomain>
2021-12-11 01:28:49 +0000

    Documents 0.6.10

--- module/lib/utils.js
+++ module/lib/utils.js
@@ -2903,7 +2903,7 @@ const dc_utils = {
                 }
                 return `
                     <div class="telegram">
-                        <div class="telegram-date">
+                        <div class="telegram-stamp-box">
                             <img class="telegram-stamp" src="https://cdn.pixabay.com/photo/2014/04/02/10/24/stamp-303749_960_720.png">
                             <p class="telegram-date-text">${data.date}</p>
                         </div>
