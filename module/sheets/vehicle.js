a3e5784777aa252b986d2841b34cf196f6bf563c
RhombusWeasel <pete@Jarvis.localdomain>
2021-12-15 21:37:03 +0000

    Vehicles 0.2.18

--- module/sheets/vehicle.js
+++ module/sheets/vehicle.js
@@ -23,17 +23,19 @@ export default class VehicleSheet extends ActorSheet {
         data.melee_weapons = dc_utils.char.items.get(this.actor, "melee");
         data.firearms      = dc_utils.char.items.get(this.actor, "firearm", "gun_type");
         data.goods         = dc_utils.char.items.get(this.actor, "goods");
-        $("#handle1").roundSlider({
-            sliderType: "min-range",
-            editableTooltip: false,
-            radius: 35,
-            width: 8,
-            value: this.actor.data.data.speed,
-            handleSize: 0,
-            handleShape: "square",
-            circleShape: "pie",
-            startAngle: 315,
-        });
+        if($("#handle1")) {
+            $("#handle1").roundSlider({
+                sliderType: "min-range",
+                editableTooltip: false,
+                radius: 35,
+                width: 8,
+                value: this.actor.data.data.speed,
+                handleSize: 0,
+                handleShape: "square",
+                circleShape: "pie",
+                startAngle: 315,
+            });
+        }
         return data;
     }
 
