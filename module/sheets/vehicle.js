c3961f7934f8e80dd1630888d889cdf4d3418875
RhombusWeasel <pete@Jarvis.localdomain>
2021-12-15 23:00:48 +0000

    Vehicles 0.2.26

--- module/sheets/vehicle.js
+++ module/sheets/vehicle.js
@@ -17,6 +17,9 @@ export default class VehicleSheet extends ActorSheet {
             handleSize: "+16",
             tooltipFormat: function (args) {
                 return args.value + "°";
+            },
+            change: function(args) {
+                $("#handle1").roundSlider('setValue', this.actor.data.data.speed);
             }
         });
         $("#handle1").roundSlider({
@@ -53,7 +56,6 @@ export default class VehicleSheet extends ActorSheet {
         data.melee_weapons = dc_utils.char.items.get(this.actor, "melee");
         data.firearms      = dc_utils.char.items.get(this.actor, "firearm", "gun_type");
         data.goods         = dc_utils.char.items.get(this.actor, "goods");
-        $("#handle1").roundSlider('setValue', this.actor.data.data.speed);
         return data;
     }
 
