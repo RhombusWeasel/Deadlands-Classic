31cf00a39db808227152f01985642ddcb8d1837d
RhombusWeasel <pete@Jarvis.localdomain>
2022-01-02 15:31:51 +0000

    Bugfix 0.1.2

--- module/socket.js
+++ module/socket.js
@@ -216,7 +216,7 @@ let operations = {
     toggle_light: function(data) {
         if (game.user.isGM) {
             let tkn = dc_utils.get_token(data.name);
-            tkn.update({
+            dc_utils.random_update(tkn, {
                 light: {
                     bright: data.bright_light,
                     dim: data.dim_light,
